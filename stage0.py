586bafeea2940a27cca650a7f5c0ccec0e48894e
asmaebt <asmboutk@cisco.com>
2021-03-12 10:32:23 +0100

    retreive ntp policies

---
+++ stage0.py
@@ -0,0 +1,21 @@
+from utils.auth import IntersightAuth
+import requests
+import json
+from env import config
+
+auth = IntersightAuth(secret_key_filename=config['INTERSIGHT_CERT'],
+                      api_key_id=config['INTERSIGHT_API_KEY'])
+
+BASE_URL = 'https://www.intersight.com/api/v1'
+
+url = f"{BASE_URL}/cond/Alarms"
+
+response = requests.get(url, auth=auth)
+
+# retreive ntp policies
+ntp_url = f"{BASE_URL}/ntp/Policies"
+headers = {
+    "Accept": "application/json"
+}
+ntp_resp = requests.get(ntp_url, auth=auth, headers=headers)
+print(json.dumps(ntp_resp.json(), indent=4))
